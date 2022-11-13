daf411fb55a29cc03087211dc9d2ae7eec0c9af9
Fraggod <102302326+Fraggod@users.noreply.github.com>
2022-11-13 22:48:12 +0530

    Add files via upload

--- CodexRobot/__main__.py
+++ CodexRobot/__main__.py
@@ -781,7 +781,7 @@ def main():
     dispatcher.add_handler(settings_handler)
     dispatcher.add_handler(help_callback_handler)
     dispatcher.add_handler(settings_callback_handler)
-    dispatcher.add_handler(donate_handler)
+    
 
     dispatcher.add_error_handler(error_callback)
 
