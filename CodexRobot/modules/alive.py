4f3ef1cbb6cd9792dbf5630b7f4e4d7798bd8273
Fraggod <102302326+Fraggod@users.noreply.github.com>
2022-11-13 11:53:30 +0530

    Add files via upload

---
+++ CodexRobot/modules/alive.py
@@ -0,0 +1,35 @@
+import random
+
+from pyrogram import __version__ as pyrover
+from telegram import __version__ as telever
+from telethon import Button
+from telethon import __version__ as tlhver
+
+from CodexRobot import OWNER_USERNAME, SUPPORT_CHAT, dispatcher
+from CodexRobot import telethn as tbot
+from CodexRobot.events import register
+
+PHOTO = [
+    "https://te.legra.ph/file/fc956a798c9b27a7d09e6.jpg",
+    "https://te.legra.ph/file/fc956a798c9b27a7d09e6.jpg",
+]
+
+
+@register(pattern=("/alive"))
+async def awake(event):
+    TEXT = f"**ʜᴇʏ​ [{event.sender.first_name}](tg://user?id={event.sender.id}),\n\nɪ ᴀᴍ {dispatcher.bot.first_name}**\n━━━━━━━━━━━━━━━━━━━\n\n"
+    TEXT += f"» **ᴍʏ ᴅᴇᴠᴇʟᴏᴘᴇʀ​ : [𝙱𝙻𝙰𝙲𝙺 𝙷𝙰𝚃](https://t.me/{OWNER_USERNAME})** \n\n"
+    TEXT += f"» **ʟɪʙʀᴀʀʏ ᴠᴇʀsɪᴏɴ :** `{telever}` \n\n"
+    TEXT += f"» **ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `{tlhver}` \n\n"
+    TEXT += f"» **ᴘʏʀᴏɢʀᴀᴍ ᴠᴇʀsɪᴏɴ :** `{pyrover}` \n━━━━━━━━━━━━━━━━━\n\n"
+    BUTTON = [
+        [
+            Button.url("ʜᴇʟᴘ​", f"https://t.me/{dispatcher.bot.username}?start=help"),
+            Button.url("sᴜᴘᴘᴏʀᴛ​", f"https://t.me/{SUPPORT_CHAT}"),
+        ]
+    ]
+    ran = random.choice(PHOTO)
+    await tbot.send_file(event.chat_id, ran, caption=TEXT, buttons=BUTTON)
+
+
+__mod_name__ = "Aʟɪᴠᴇ"
