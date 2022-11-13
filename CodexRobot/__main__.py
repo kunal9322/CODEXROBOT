e317f8f49da84076ae713503f94aa04a7f58ad7a
Fraggod <102302326+Fraggod@users.noreply.github.com>
2022-11-13 22:17:40 +0530

    Add files via upload

--- CodexRobot/__main__.py
+++ CodexRobot/__main__.py
@@ -772,8 +772,7 @@ def main():
         Source_about_callback, pattern=r"source_"
     )
 
-    donate_handler = CommandHandler("donate", donate)
-    migrate_handler = MessageHandler(Filters.status_update.migrate, migrate_chats)
+    
 
     dispatcher.add_handler(start_handler)
     dispatcher.add_handler(help_handler)
